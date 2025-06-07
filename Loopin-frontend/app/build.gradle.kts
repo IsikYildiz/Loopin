44c1fd46336f157e8cf795dcd9c3d9e42f0c55c9
hasanylld <hasanyildiz816@gmail.com>
2025-06-07 19:50:02 +0300

    bio öne çıkarıldı ve daha dikkat çekici hale getirildi

--- Loopin-frontend/app/build.gradle.kts
+++ Loopin-frontend/app/build.gradle.kts
@@ -65,4 +65,5 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.15.1")
     kapt("com.github.bumptech.glide:compiler:4.15.1")
     implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
